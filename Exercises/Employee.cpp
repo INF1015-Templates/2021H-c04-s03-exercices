b4f7af9a71d2580a3681e11a3baac944598da0e1
chosson <chosson@users.noreply.github.com>
2021-02-03 22:55:26 -0500

    Removed answers.

--- Exercises/Employee.cpp
+++ Exercises/Employee.cpp
@@ -13,32 +13,5 @@
 
 using namespace std;
 
-
-//Employee::Employee() {
-//	name_ = "";
-//	salary_ = 0.0;
-//	cout << "Employee()" << endl;
-//}
-
-Employee::Employee(const string& name, double salary)
-: name_(name),
-  salary_(salary) {
-	cout << "Employee(const string&, double)" << endl;
-}
-
-Employee::~Employee() {
-	cout << "~Employee()" << endl;
-}
-
-const string& Employee::getName() const {
-	return name_;
-}
-
-double Employee::getSalary() const {
-	return salary_;
-}
-
-void Employee::setSalary(double salary) {
-	salary_ = salary;
-}
+// TODO: Les définitions.
 
